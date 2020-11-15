7288e9b50bbd4b1b2cd84da2288fb4034d1e0b00
umunadine <umutesinad3@gmail.com>
2020-11-15 18:47:13 +0200

    added docstrings

--- password.py
+++ password.py
@@ -6,10 +6,12 @@ class User:
     user_list = [] # Empty user list
 
     def __init__(self,username,password):
-
-      # docstring removed for simplicity
-
         self.username = username
         self.password = password
+
+
     def save_user(self):
+     """
+    A method that saves a new user.
+     """
         User.user_list.append(self)
