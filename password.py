948aeb9eea18a3f07ea78b39d404ac39b857ecdf
umunadine <umutesinad3@gmail.com>
2020-11-15 18:45:01 +0200

    creating a user class

---
+++ password.py
@@ -0,0 +1,15 @@
+class User:
+    """
+    Class that generates new instances of user.
+    """
+    
+    user_list = [] # Empty user list
+
+    def __init__(self,username,password):
+
+      # docstring removed for simplicity
+
+        self.username = username
+        self.password = password
+    def save_user(self):
+        User.user_list.append(self)
