870829f627e2e464f6d239f8b9818e708a611ee5
umunadine <umutesinad3@gmail.com>
2020-11-15 20:22:29 +0200

    testing the delete

--- test_password.py
+++ test_password.py
@@ -33,5 +33,17 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.user_list),1)
 
 
+    def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user") # new contact
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)
+
+
 if __name__ == '__main__':
     unittest.main()
