3e2111045cc367bc88fe443c72d844af32863b11
umunadine <umutesinad3@gmail.com>
2020-11-16 19:00:26 +0200

    Creating credentials

--- run.py
+++ run.py
@@ -85,7 +85,42 @@ def main():
         save_user(create_new_user(username,password))
         print('-' * 50)
         print(f"Hello {username}, Your account has been created succesfully! Your password is: {password}")
-    
+    elif short_code == "b":
+        print("Login to your account")
+        print('-'*50)
+        print("login_username:")
+        username = input()
+        print("login_password:")
+        password = input()
+        login = login_user(username,password)
+        print('\n')
+        print(f"Hello {username}.Welcome To PassWord Locker Manager")  
+        print("Choose between the following:\n CC - Create a new credential \n DC - Display Credentials \n FC - Find a credential \n DC - Delete credential \n EX - Exit the application \n")
+        short_code=input("").lower().strip()
+        if short_code == "cc":
+            print("Creating a new credential")
+            print('-'*50)
+            print("Account Name ")
+            accname = input().lower()
+            print("Username ")
+            userName = input()
+            print("Password")
+            password = input()
+            save_credential(create_new_credential(accname,userName,password))
+            print(f"Account Credential for: {accname} - UserName: {userName} - Password:{password} created succesfully")
+            print("\n")
+        elif short_code == "dc":
+            print("Display Credentials")
+        elif short_code == "fc":
+            print("Finding Credentials")
+        elif short_code == "dc":
+            print("Deleting Credentials")
+        elif short_code == "ex":
+            print("Thanks for using password locker")
+        else:
+            print("Invalid")
+        
+
               
 
 
